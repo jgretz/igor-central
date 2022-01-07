c4449ee1d268166ffeaefb0be1dd7a075e7778c3
Josh Gretz <jgretz@truefit.io>
2022-01-07 15:48:01 -0500

    add rabbitmq provider to module def

--- src/central.module.ts
+++ src/central.module.ts
@@ -1,9 +1,10 @@
 import {Module} from '@nestjs/common';
+import {RabbitMqService} from '@jgretz/igor-rabbit';
 import {CrudController, CommandController} from './controllers';
 
 @Module({
   imports: [],
   controllers: [CrudController, CommandController],
-  providers: [],
+  providers: [RabbitMqService],
 })
 export class CentralModule {}
