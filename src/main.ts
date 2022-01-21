4c50af7dc5f54c8715369d674483513b0663c48a
Josh Gretz <jgretz@truefit.io>
2022-01-21 10:26:50 -0500

    break out to own app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import {NestFactory} from '@nestjs/core';
 import {CentralModule} from './central.module';
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(CentralModule);
   app.startAllMicroservices();
 }
+bootstrap();
